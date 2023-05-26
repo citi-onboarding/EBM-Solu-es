1f40dcaeb1814468ff8157cedf88332f4f2b9d02
Victória Queiroz <104035388+victoriaxq@users.noreply.github.com>
2023-05-26 14:06:26 -0300

    routes.ts

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -9,9 +9,4 @@ routes.get('/user', userController.get);
 routes.delete('/user/:id', userController.delete);
 routes.put('/user/:id', userController.update);
 
-routes.post('/image', imageController.create);
-routes.get('/image', imageController.get);
-routes.delete('/image/:id', imageController.delete);
-routes.put('/image/:id', imageController.update);
-
 export default routes;
