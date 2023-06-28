4f43d071dd55379302bbef2040ec8875f410cd60
weslley-batista <wbh@cin.ufpe.br>
2023-06-28 12:32:36 -0300

    feature/contato: frontEnd contato

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -12,17 +12,18 @@ import { Home } from '../../client/src/pages/home';
 import { FooterScreen } from '../../client/src/pages/footer';
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
-import Carousel from '../../client/src/components/carrosel'
+import Carousel from '../../client/src/components/carrosel';
 function App() {
   return (
     <ThemeProvider theme={theme}>
-      <Nav/>
-      <Home/>
-      <SobreNos/>
-      <Service/>
-      <Carrossel/>
-      <NossosClientes/>
-      <FooterScreen/>
+        <Nav />
+        <Home />
+        <SobreNos />
+        <Service />
+        <Carrossel />
+        <NossosClientes />
+        <FooterScreen />
+        <GlobalStyle/>
     </ThemeProvider>
   );
 }
--- client/src/pages/BarraNavegação/styles.ts
+++ client/src/pages/BarraNavegação/styles.ts
@@ -15,6 +15,7 @@ export const Ebm = styled.div`
 export const Card = styled.div`
     display: flex;
     justify-content: center;
+    align-items: center;
     width: 135px;
     height: 49px;
     margin-top: 27px;
@@ -30,8 +31,9 @@ export const Texto = styled.p`
    font-style: normal;
    font-weight: 700;
    font-size: 18px;
-   line-height: 19px;
    color: #FFFFFF;
+
+   
 `;
 
 export const Texto2 = styled.p`
@@ -39,9 +41,12 @@ export const Texto2 = styled.p`
    font-style: normal;
    font-weight: 400;
    font-size: 20px;
-   line-height: 21px;
    color: #373737;
    margin: 0%;
+    &:hover {
+        border-bottom: 2px solid #B85941;;
+        display: inline;
+    }
 `;
 
 export const Preto = styled.div`
